2948730e653c8afb357eb4464ef8b26570b126b2
Matt Macdonald Rasp4 <mksmacdonald@gmail.com>
2021-04-27 14:32:13 -0700

    4_queries done

--- 4_queries_assistance/92_name_of_teachers_and_total_assistances.sql
+++ 4_queries_assistance/92_name_of_teachers_and_total_assistances.sql
@@ -1,7 +1,19 @@
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM cohorts
-JOIN students on  cohorts.id = cohort_id
-JOIN assistance_requests on students.id = assistance_requests.student_id
-JOIN teachers on teacher_id = teachers.id
+-- SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT (assistance_requests) as total_assistances
+-- FROM cohorts
+-- JOIN students on  cohorts.id = cohort_id
+-- JOIN assistance_requests on students.id = assistance_requests.student_id
+-- JOIN teachers on teacher_id = teachers.id
+-- WHERE cohorts.name = 'JUL02'
+-- ORDER BY teacher
+
+
+--  apparently we can group by more than one thing. Its not intuitive, but at least we know now
+
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
-ORDER BY teacher
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
