f981301e37f0817a229e0d2aad0dfa74ce4e91b2
Matt Macdonald Rasp4 <mksmacdonald@gmail.com>
2021-04-29 14:18:45 -0700

    dont wrap $1 in quotes

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,27 +1,31 @@
-const { Pool } = require('pg');
-const cliVAR = process.argv.slice(2);
+const { Pool } = require("pg");
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
 const pool = new Pool({
-  user: 'pi',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "pi",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
 });
 
-
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM cohorts
 JOIN students on  cohorts.id = cohort_id
 JOIN assistance_requests on students.id = assistance_requests.student_id
 JOIN teachers on teacher_id = teachers.id
-WHERE cohorts.name = '${process.argv[2]}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(data => {
-    console.log(`${data.cohort}:  ${data.teacher}`);
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((data) => {
+      console.log(`${data.cohort}:  ${data.teacher}`);
+    });
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch((err) => console.error("query error", err.stack));
